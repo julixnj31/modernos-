8afba670a73d80fc3a516d7b36478284a0314662
Yoinel Martinez <yoinelmartinez04@gmail.com>
2026-04-17 13:21:44 -0500

    arregle el punto 1 e hice el punto 2 apropiacion. refs #3

--- apropiacion/punto1.js
+++ apropiacion/punto1.js
@@ -2,13 +2,22 @@ function procesarPedido(pedido, ...extras) {
     const { cliente, producto, cantidad } = pedido;
 
     const pedidoFinal = {
-    cliente,
-    producto,
-    cantidad,
-    extras,
-    totalExtras: extras.length,
+        cliente,
+        producto,
+        cantidad,
+        extras,
+        totalExtras: extras.length,
     };
 
     console.log("Pedido procesado:", pedidoFinal);
     return pedidoFinal;
 }
+
+// prueba
+const pedido = {
+    cliente: "Yoinel",
+    producto: "Laptop",
+    cantidad: 2
+};
+
+procesarPedido(pedido, "Mouse", "Teclado", "Audífonos");
